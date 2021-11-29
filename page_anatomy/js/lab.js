c037e6c73b5007ea1d6069a1cb37fd380ec4d6f9
AvianBen <34182986+AvianBen@users.noreply.github.com>
2021-11-29 02:39:36 -0800

    website prototype
    the first iteration

---
+++ page_anatomy/js/lab.js
@@ -0,0 +1,9 @@
+/**
+* Author: Ben Chavez <berchave@ucsc.edu>
+* Created: October 25th 2021
+* License: Public Domain
+**/
+
+$("#full").click(function(){
+  $("#full").toggleClass("invis");
+});
---
+++ page_meme/js/lab.js
@@ -0,0 +1,10 @@
+/**
+* Author: Ben Chavez <berchave@ucsc.edu>
+* Created: October 25th 2021
+* License: Public Domain
+**/
+
+$("#button").click(function(){
+  $("#top").append($("#top1").val());
+  $("#bottom").append($("#bottom1").val());
+});
